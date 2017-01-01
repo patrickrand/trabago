918dc12eeed74c29a14037d978c8f75bba7f0ece
patrickrand <prand0001@gmail.com>
2017-01-01 12:48:54 -0500

    updated package name to reflect project

--- ratelimiter.go
+++ ratelimiter.go
@@ -1,9 +1,13 @@
-package work
+package trabago
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // A RateLimiter only allows a code block to be executed N times in a give duration of time.
 type RateLimiter struct {
+	mu       *sync.Mutex
 	n        int           // number of times
 	duration time.Duration // window of time to buffer on
 	buffer   []time.Time   // linked-list would probably be more efficient...
@@ -11,6 +15,7 @@ type RateLimiter struct {
 
 func NewRateLimiter(n int, duration time.Duration) *RateLimiter {
 	return &RateLimiter{
+		mu:       new(sync.Mutex),
 		n:        n,
 		duration: duration,
 		buffer:   make([]time.Time, 0),
@@ -18,6 +23,9 @@ func NewRateLimiter(n int, duration time.Duration) *RateLimiter {
 }
 
 func (rl *RateLimiter) IsRateLimited() bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
 	if len(rl.buffer) < rl.n {
 		rl.buffer = append(rl.buffer, time.Now())
 		return false
--- workerpool.go
+++ workerpool.go
@@ -1,4 +1,4 @@
-package work
+package trabago
 
 import "sync"
 
