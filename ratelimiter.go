c64da4783eb12482820c3a3feee711b1b8eaebf4
patrickrand <prand0001@gmail.com>
2017-01-01 14:09:47 -0500

    changed rate limiter's buffer to linkedlist, instead of slice

--- ratelimiter.go
+++ ratelimiter.go
@@ -5,16 +5,22 @@ import (
 	"time"
 )
 
+type timeNode struct {
+	t    time.Time
+	next *timeNode
+}
+
 // TODO:
-// - buffer should be linked-list, as opposed to a slice
 // - create another function, that accepts a func(...interface{}) interface{}, to be executed if not rate-limited
 
 // A RateLimiter only allows a code block to be executed N number of times, in a give duration of time.
 type RateLimiter struct {
 	mu       *sync.Mutex
 	n        int           // number of times
-	duration time.Duration // window of time to buffer on
-	buffer   []time.Time   // linked-list would probably be more efficient...
+	duration time.Duration // window of time to buffer
+	bufHead  *timeNode
+	bufTail  *timeNode
+	bufSize  int
 }
 
 // NewRateLimiter returns a new rate limiter.
@@ -23,7 +29,9 @@ func NewRateLimiter(n int, duration time.Duration) *RateLimiter {
 		mu:       new(sync.Mutex),
 		n:        n,
 		duration: duration,
-		buffer:   make([]time.Time, 0),
+		bufHead:  new(timeNode),
+		bufTail:  new(timeNode),
+		bufSize:  0,
 	}
 }
 
@@ -32,15 +40,29 @@ func (rl *RateLimiter) IsRateLimited() bool {
 	rl.mu.Lock()
 	defer rl.mu.Unlock()
 
-	if len(rl.buffer) < rl.n {
-		rl.buffer = append(rl.buffer, time.Now())
-		return false
-	}
-
-	if time.Since(rl.buffer[0]) > rl.duration {
-		rl.buffer = append(rl.buffer[1:], time.Now())
+	// allow execution if buffer is below capacity, or enough time has passed
+	if rl.bufSize < rl.n || time.Since(rl.bufHead.t) > rl.duration {
+		rl.push(time.Now())
 		return false
 	}
 
 	return true
+}
+
+func (rl *RateLimiter) push(t time.Time) {
+	if rl.bufSize == 0 || rl.bufHead == rl.bufTail || (rl.bufHead == nil || rl.bufTail == nil) {
+		rl.bufHead = &timeNode{t: t}
+		rl.bufTail = rl.bufHead
+		rl.bufSize = 1
+		return
+	}
+
+	rl.bufTail.next = &timeNode{t: t}
+	rl.bufTail = rl.bufTail.next
+	rl.bufSize++
+
+	if rl.bufSize > rl.n {
+		rl.bufHead = rl.bufHead.next
+		rl.bufSize--
+	}
 }
